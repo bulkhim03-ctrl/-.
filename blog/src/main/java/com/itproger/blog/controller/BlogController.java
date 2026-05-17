cdc8bedb1ee89c7d0e2503ee5b3c0e933735f7b0
bulkhim03-ctrl <bulkhim03@gmail.com>
2026-05-17 13:51:56 +0300

    Создание раздела блог
    заного загружаю

---
+++ blog/src/main/java/com/itproger/blog/controller/BlogController.java
@@ -0,0 +1,15 @@
+package com.itproger.blog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BlogController {
+
+    @GetMapping("/blog")
+    public String blogMain (Model model){
+        return "blogmain";
+    }
+    
+}
